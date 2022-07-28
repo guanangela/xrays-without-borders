ceacc2d09965ae6160e810ec20d3e6d4086fa2bb
Angela Guan <guanangela@berkeley.edu>
2022-07-28 00:28:11 -0700

    about and result

--- app.py
+++ app.py
@@ -62,7 +62,6 @@ def team():
     return render_template("team.html")
 
 
-
 # Main method that handles a user upload request.
 # When an image is sent it:
 # 1. Ensures the image is a valid image
@@ -97,9 +96,9 @@ def upload_file():
             # 2. Save the image to a path in the server's directory.
             # NOTE: This is not the safest thing to do.
             saved_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'images/' + filename)
-            print(saved_filename)
+            # print(saved_filename)
             file.save(saved_filename)
-            print(filename)
+            # print(filename)
 
             # 3. Run inference.
             with open(saved_filename, 'rb') as f:
@@ -108,65 +107,23 @@ def upload_file():
             img = prepare_image(image_bytes)
             # return jsonify(prediction=predict_result(img))
             prediction = predict_result(img)
-            print('prediction->>>>>>>>>>>>>>>>>>>>>>>', prediction)
+            # print('prediction->>>>>>>>>>>>>>>>>>>>>>>', prediction)
 
             # for i in range(len(labels)):
                 # grad_cam_path = 'static/images/grad_cam.png' 
             grad_cam_path_1 = 'static/images/grad_cam_1.png'
             grad_cam_path_2 = 'static/images/grad_cam_2.png'
             pred_prob_1 = compute_gradcam(model, saved_filename, 'Cardiomegaly', grad_cam_path_1, layer_name='bn')
-            pred_prob_2 = compute_gradcam(model, saved_filename, 'No Finding', grad_cam_path_2, layer_name='bn')
+            # pred_prob_2 = compute_gradcam(model, saved_filename, 'No Finding', grad_cam_path_2, layer_name='bn')
             if pred_prob_1 > 0.5:
                 predicted_label = 'Cardiomegaly'
+                return render_template("result.html", prediction_val = predicted_label, saved_filename = 'images/' + filename, gradcam_heatmap_1 = 'images/grad_cam_1.png') #, gradcam_heatmap_2 = 'images/grad_cam_2.png')    
+
             else:
                 predicted_label = 'No Finding'
-            return render_template("result.html", prediction_val = predicted_label, saved_filename = 'images/' + filename, gradcam_heatmap_1 = 'images/grad_cam_1.png', gradcam_heatmap_2 = 'images/grad_cam_2.png')    
+                compute_gradcam(model, saved_filename, 'No Finding', grad_cam_path_2, layer_name='bn')
+                return render_template("result.html", prediction_val = predicted_label, saved_filename = 'images/' + filename, gradcam_heatmap_1 = 'images/grad_cam_2.png') #, gradcam_heatmap_2 = 'images/grad_cam_2.png')    
         return render_template('index.html')
-
-
-        # file = request.files['file']
-        # print(file)
-
-        # if not file:
-        #     return
-        # filename = secure_filename(file.filename)
-        # saved_filename = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        # file.save(saved_filename)
-        # print(filename)
-        # img_bytes = file.read()
-        # img = prepare_image(img_bytes)
-
-        # # return jsonify(prediction=predict_result(img))
-        # prediction = predict_result(img)
-        # return render_template("result.html", prediction_val = prediction, saved_filename = filename)    
-
-
-
-        # 1. Ensure the image exists.
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        # if file.filename == '':
-        #     print('No selected file.')
-        #     return redirect(request.url)
-
-        # # If we're here, the user uploaded the correct file.
-        # if file and allowed_file(file.filename):
-        #     filename = secure_filename(file.filename)
-        #     # 2. Save the image to a path in the server's directory.
-        #     # NOTE: This is not the safest thing to do.
-        #     saved_filename = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        #     file.save(saved_filename)
-        #     print(filename)
-
-        #     # 3. Run inference.
-        #     class_id, class_name = infer_image(saved_filename)
-        #     class_name = class_name.replace('_', ' ')
-        
-
-        # 4. Send the new image back to the user.
-    #     return render_template("result.html", class_id = class_id, class_name = class_name, saved_filename = filename)    
-    # return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
